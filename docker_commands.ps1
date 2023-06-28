5e92d57a5de0f2b9cc39895139ba0a9a7af06927
Ohad <eliasiohad@gmail.com>
2023-06-29 01:57:38 +0300

    docker compose

---
+++ docker_commands.ps1
@@ -0,0 +1,12 @@
+docker compose build
+docker-compose up -d
+docker-compose stop to stop containers
+docker-compose rm to remove containers
+docker-compose down to stop and remove containers
+
+docker login
+docker ps
+docker images
+docker push
+
+docker system prune -a
